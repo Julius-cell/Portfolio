81ffacd5f7325fa617e2989a8dcaba34f0456476
juliocid <julio.cid@femsasalud.com>
2024-11-13 17:14:57 +0000

    add home and not found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pg-not-found',
+  templateUrl: './not-found.component.html',
+  styles: [],
+})
+export class NotFoundComponent {}
